e40d05b637cc82adbbb89b428567d14bcd6a2b1a
javascriptsucks <evix1988@live.cn>
2022-12-12 17:24:44 -0600

    mongodb-config

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require(`${__dirname}/app.js`);
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection established!!!'));
 
 console.log(app.get('env'));
 const port = process.env.PORT || 3000;
