73e23c3132ac6168ebb7c7663368392c450949fa
javascriptsucks <evix1988@live.cn>
2023-01-02 11:43:20 -0600

    embeding-user-data-in-tour-data

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('../models/userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -98,7 +99,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   { toJSON: { virtuals: true } },
   { toObject: { virtuals: true } }
@@ -113,6 +115,12 @@ tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
+tourSchema.pre('save', async function(next) {
+  const guidesPromise = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromise);
+  next();
+});
+
 // tourSchema.pre('save', (next) => {
 //   console.log('Will Save Document');
 //   next();
